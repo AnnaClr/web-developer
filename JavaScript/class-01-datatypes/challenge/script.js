0a8c31f3d2213615a4c0ccbf81d4646a84109a99
Ana Clara <Annaclarag2005@gmail.com>
2022-11-07 15:11:41 -0300

    add java

---
+++ JavaScript/class-01-datatypes/challenge/script.js
@@ -0,0 +1,3 @@
+// Faça um programa que solicite (prompt) o nome do usaario, de as boa vindas e avise que irá fazer uma soma: Ex: Bem vindo "nome", vamos fazer uma soma!
+
+// Apresente a soma dos numeros com "alert" na tela: Ex: A soma do "numero1" e o "numero2" é "soma"
