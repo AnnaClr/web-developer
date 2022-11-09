164ccda2cb239c9fd57b506ee748507e734f25aa
Ana Clara <Annaclarag2005@gmail.com>
2022-11-09 15:39:02 -0300

    add js

---
+++ JavaScript/class-02-operators/study/script.js
@@ -0,0 +1,43 @@
+// Binary
+let n1 = 8
+let n2 = 7
+
+// console.log(n1 + n2)
+// console.log("test " + n2)
+// console.log(n1 + "5")
+
+// Unary
+
+// console.log(n1)
+// console.log(++n1)
+// console.log(n1++)
+
+// console.log(typeof n1)
+
+// const fruits = ["Banana", "Mamão", "Uva"]
+// console.log(fruits)
+
+// Temary
+
+// let avarege = 7.0
+// console.log(avarege >= 7 ? 'Aprovado': 'Reprovado')
+// console.log(true ? 'Aprovado': 'Reprovado')
+// console.log(false ? 'Aprovado': 'Reprovado')
+
+
+
+// console.log(7 + 8 / 2)
+// console.log((7 + 8) / 2)
+// console.log(3 > 2 && 4 < 5)
+// console.log(3 > 2 || 4 < 5)
+// console.log(3 > 2 )
+// console.log(true == 1)
+// console.log(3 > 2 == 1)
+// console.log(3 > 2 && 2 > 1)
+// console.log(true === 1)
+
+
+
+
+
+
