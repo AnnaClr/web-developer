5d9d4b5fe81e6fe672c72bbc46d0b0ebc1d99b66
Ana Clara <Annaclarag2005@gmail.com>
2022-11-09 15:06:21 -0300

    add js

---
+++ JavaScript/class/study/script.js
@@ -0,0 +1,24 @@
+// Binary
+let n1 = 8
+let n2 = 7
+
+// console.log(n1 + n2)
+// console.log("test " + n2)
+// console.log(n1 + "5")
+
+// Unary
+
+// console.log(n1)
+// console.log(++n1)
+// console.log(n1++)
+
+// console.log(typeof n1)
+
+// const fruits = ["Banana", "Mamão", "Uva"]
+// console.log(fruits)
+
+// Temary
+
+let avarege = 7
+console.log(avarege >= 7 ? 'Aprovado': 'Reprovado')
+
