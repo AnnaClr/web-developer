c6e47c7c9489ac98b7ca111184a4a7a35886ecdb
Ana Clara <Annaclarag2005@gmail.com>
2022-11-10 15:23:02 -0300

    add js

--- JavaScript/class-02-operators/study/script.js
+++ JavaScript/class-02-operators/study/script.js
@@ -1,6 +1,6 @@
 // Binary
-let n1 = 8
-let n2 = 7
+// let n1 = 8
+// let n2 = 7
 
 // console.log(n1 + n2)
 // console.log("test " + n2)
---
+++ JavaScript/class-03-functions/script.js
@@ -0,0 +1,43 @@
+// Functions Statements
+
+// >>> Named <<<
+
+// function myTest(text) { // Parameters
+//     console.log(text)
+// }
+
+// myTest("Testando o texto")
+
+//-------------------------------
+
+// function somar(num1, num2) {
+//    let soma = num1 + num2 // Atenção para o operador chave
+//     return soma
+// }
+
+// console.log(somar(20, 30))
+// console.log(soma)
+
+
+// >>> Anonymous <<<
+ 
+// let sum = function (num1, num2) {
+//     let soma = num1 + num2 // Atenção para o operador chave
+//      return soma
+//  }
+
+// console.log(sum(2, 3))
+
+
+// >>> Arrow <<<
+
+let sum = (num1, num2) => num1 + num2
+console.log(sum(2, 3))
+
+
+
+
+
+
+
+
