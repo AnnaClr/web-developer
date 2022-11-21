ca2a44fc6ca7d8fe9842c6154edf5de2fc88e7a7
Ana Clara <Annaclarag2005@gmail.com>
2022-11-21 16:16:23 -0300

    add js

--- JavaScript/class-05-conditional/study/script.js
+++ JavaScript/class-05-conditional/study/script.js
@@ -8,7 +8,11 @@
 //     window.alert("Corre pessoal!")
 // }
 
-let age = 16
+// let age = Math.abs(Number(prompt("Digite sua idade:")))
+// if (String(age).length >= 3 ) {
+//     console.log("Sua idade está correta?")
+
+// } 
 // if (age >= 18) {
 //     console.log("Maior de idade!")
 //     if (cartão) {
@@ -19,12 +23,29 @@ let age = 16
 // } else {
 //     console.log("Menor de idade!")
 // }
+// console.log(age)
+// if (age >= 16) {
+//     if ( age >=18 && age <=70) {
+//     console.log("Voto Obrigatório!")
+//     } else {
+//     console.log("Voto facultativo!")
+//     } 
+// } else {
+//     console.log("Não Pode votar!")
 
-if (age >= 16) {
-    console.log("Pode votar!")
-} else {
-    console.log("Não pode votar!")
-}
+//     switch (key) {
+//         case value:
+
+//         break;
+
+//         case value:
+//     }
+// }
+
+//     console.log("Pode votar!")
+// } else {
+//     console.log("Não pode votar!")
+// }
 
 
 
