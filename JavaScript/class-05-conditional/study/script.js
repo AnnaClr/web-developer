e256270aa82398bb59adf1167d830116e9126a9f
Ana Clara <Annaclarag2005@gmail.com>
2022-11-22 15:22:53 -0300

    add js

--- JavaScript/class-05-conditional/study/script.js
+++ JavaScript/class-05-conditional/study/script.js
@@ -33,41 +33,50 @@
 // } else {
 //     console.log("Não Pode votar!")
 
-let weekDay = 1 // 1 - 7
-let day
+// let weekDay = 1 // 1 - 7
+// let day
 
-    switch (weekDay) {
-        case 1:
-            day = "Domingo"
-        break;
-        case 2:
-            day = "Segunda"
-        break;
-        case 3:
-            day = "Terça"
-        break;
-        case 4: 
-             day = "Quarta"
-        break;
-        case 5:
-            day = "Quinta"
-        break;
-        case 6:
-             day = "Sexta"
-        break;
-        case 7: 
-              day = "Sábado"
+//     switch (weekDay) {
+//         case 1:
+//             day = "Domingo"
+//         break;
+//         case 2:
+//             day = "Segunda"
+//         break;
+//         case 3:
+//             day = "Terça"
+//         break;
+//         case 4: 
+//              day = "Quarta"
+//         break;
+//         case 5:
+//             day = "Quinta"
+//         break;
+//         case 6:
+//              day = "Sexta"
+//         break;
+//         case 7: 
+//               day = "Sábado"
 
-        default:
-            // console.log("Dia não encontrado!")
-            day = "Dia não encontrado!"
-        break;
-    }
+//         default:
+//             // console.log("Dia não encontrado!")
+//             day = "Dia não encontrado!"
+//         break;
+//     }
     
-    console.log(day)
+// console.log(day)
 
 
+// Try, Catch, Throw
 
+// let myFunction = () => "Running..."
+// try {
+//     throw new Error(`Número fora do intervalo de 1 a 7`)
+//     myFunction
+// } catch (error) {
+//     console.log(`${error}`)
+// }
 
+// document.write("Rodando aplicação...")
 
 
