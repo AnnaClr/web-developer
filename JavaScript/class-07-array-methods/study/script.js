f3f8625fcf2f0fd66a9c958f391d6075dc471a4f
Ana Clara <Annaclarag2005@gmail.com>
2022-11-29 16:19:46 -0300

    add js

---
+++ JavaScript/class-07-array-methods/study/script.js
@@ -0,0 +1,22 @@
+// Array Methods - Principio da imutabilidade
+// const team = ['Brasil']
+// const teams = ['Brasil', 'Alemanha', 'Argentina', 'Portugal']
+
+// teams.forEach((value)=> {
+//     console.log("Nome:" + value)
+// }) 
+
+// let teamArray = Array.from(team)
+// team.forEach((value) => {
+//     console.log(value)
+// })
+
+
+// console.log(teamArray)
+
+// let teste = teams.map((v, i, a) => {
+//     console.log(v, i, a)
+//     return v + 'Teste'
+// })
+// console.log(teste)
+// console.log(teams)
