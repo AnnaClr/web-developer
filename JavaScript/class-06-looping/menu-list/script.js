f1f703363809d89e738a0145ae580c0dca47e7d0
Ana Clara <Annaclarag2005@gmail.com>
2022-12-14 14:44:51 -0300

    add js

--- JavaScript/class-06-looping/menu-list/script.js
+++ JavaScript/class-06-looping/menu-list/script.js
@@ -21,7 +21,6 @@ while (true) {
 
     switch (menuOption) {
         case '1':
-          
            while (true) {
             let item = prompt("Digite o nome do item")
                                     
@@ -41,7 +40,9 @@ while (true) {
                     listItens.push(item)
                     alert(`"${item} cadastrado com sucesso!"`)
                 }
-           }
+
+            }
+    
             break;
 
         case '2':
@@ -52,14 +53,44 @@ while (true) {
             }
             break;
 
+         case '3':
+                if (listItens.length == 1) {
+                    alert(`${listItens[0]} removido com sucesso!`)
+                    listItens.pop()
+                    break;
+                }
+             
+            while (true) {           
+                if (listItens.length == 0) {
+                alert('Lista vazia!')
+                break;
+             } else {
+                let item = prompt('Digite o nome para remover:') 
+                
+                if (item === null) {
+                    break;
+                }
+
+                if (item === '') {
+                    alert("Por favor, digite o nome para cadastrar!")
+                    continue;
+                }
+
+                if (listItens.includes(item)) {
+                    listItens.splice(listItens.indexOf(item), 1)
+                alert(`"${item}" foi removido!`)
+                } else {
+                    alert("Item não encontrado!")
+                }
+            }
+        }
+            break;
         default:
             break;
     
     }
-
-    console.log(menuOption)
+    console.log(listItens)
 }
-
 alert("Programa cancelado!")
 
 
