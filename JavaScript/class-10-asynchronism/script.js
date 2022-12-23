6ba91b7d2d7e75b9bdd555b283624fb9f2fbd6af
Ana Clara <Annaclarag2005@gmail.com>
2022-12-23 16:42:28 -0300

    add js

--- JavaScript/class-10-asynchronism/script.js
+++ JavaScript/class-10-asynchronism/script.js
@@ -33,5 +33,52 @@
 
 // >>> PROMISES <<< 
 
+// console.log('Requisição');
+// const myPromise = new Promise((resolve, reject) => {
+//     let request = true
+//     if (request) {
+//         return resolve('Requisição aceita!')
+//     } else {
+//         return reject('Requisição não aceita!')
+//     }
+    
+// })
 
+// console.log(myPromise);
+// myPromise.then((answer) => console.log(answer)).catch((error) => console.log(error)).finally(() => console.log('Requisição encerrada!'))
 
+// >>> ASYNC AWAIT <<<
+// async function requestData(){
+//     try {
+//         const promise = await myPromise
+//         console.log(promise);
+//     } catch (error) {
+//         console.log(error);
+//     } finally {
+//         console.log("Requisição encerrada!");
+//     }
+    
+// }
+// requestData()
+// console.log('Finalizar requisição');
+
+// >>> FETCH <<<
+// const user = 'AnnaClr'
+// const url = `https://api.github.com/users/${user}`
+// const userGitHub = fetch(url)
+// console.log(userGitHub)
+// userGitHub.then((request) => request.json()).then((name, bio) => console.log(name, bio))
+
+// async function userGitHubData(user) {
+//     try {
+//        await fetch(`https://api.github.com/users/${user}`).then((request) => request.json()).then((data) => console.log(`${data.name}: ${data.bio}`))
+       
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// userGitHubData('AnnaClr')
+// userGitHubData('EmanuelQuintino')
+// userGitHubData('jose-murilo')
+// userGitHubData('Mario13Sergio')
